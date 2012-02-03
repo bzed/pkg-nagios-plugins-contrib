f9dc263fa553ce22d0a16a60d781a390abb525d8
Bernd Zeimetz <bernd@bzed.de>
2012-02-03 12:27:17 +0100

    Update update-debian-files.py

--- debian/update-debian-files.py
+++ debian/update-debian-files.py
@@ -2,18 +2,58 @@
 
 import sys
 import os
+from debian import deb822
 
 # find all plugins
-__plugins__ = [p for p in os.listdir(os.path.realpath(os.path.dirname(sys.argv[0]) + os.path.sep + '..')) 
-                   if (os.path.isdir(p) and p!='debian' and p!='.git')]
+__basedir__ = os.path.realpath(os.path.dirname(sys.argv[0]) + os.path.sep + '..')
+__plugins__ = [p for p in os.listdir(__basedir__) 
+                   if (os.path.isdir(__basedir__ + os.path.sep + p) and p!='debian' and p!='.git')]
 __plugins__.sort()
 
-
 def update_control():
-    pass
+    control_data = {
+        'Depends' : [],
+        'Suggests' : [],
+        'Recommends' : [],
+        'Build-Depends' : [],
+        'Description' : []
+    }
+
+    for plugin in __plugins__:
+        # We look at the first paragraph only!
+        _control = [x for x in deb822.Packages.iter_paragraphs(file(__basedir__ + os.path.sep+ plugin + os.path.sep + 'control'))][0]
+        # look trough keys we might want to merge
+        for key in ['Depends', 'Build-Depends', 'Suggests', 'Recommends']:
+            if _control.has_key(key):
+                control_data[key].extend(deb822.PkgRelation.parse_relations(_control[key]))
+        # extract description
+        description = '   * %s' %(plugin,)
+        if _control.has_key('Version'):
+            description = '%s (%s)' %(description, _control['Version'])
+        description = '%s: %s' %(description, _control['Description'].replace('\n','\n    '))
+        control_data['Description'].append(description)
+
+    with open(__basedir__ + os.path.sep + 'debian' + os.path.sep + 'control.in', 'r') as f:
+        control_in = f.read()
+
+    for k, v in control_data.iteritems():
+        if k == 'Description':
+            control_in = control_in.replace('#AUTO_UPDATE_Description#', '\n'.join(v))
+        else:
+            control_in = control_in.replace('#AUTO_UPDATE_%s#' %(k, ), deb822.PkgRelation.str(v))
+
+    with open(__basedir__ + os.path.sep + 'debian' + os.path.sep + 'control', 'w') as f:
+        f.write(control_in)
+
+
+
 
 def update_copyright():
-    pass
+    with open(__basedir__ + os.path.sep + 'debian' + os.path.sep + 'copyright.in', 'r') as f:
+        copyright_in = f.read()
+
+    with open(__basedir__ + os.path.sep + 'debian' + os.path.sep + 'copyright', 'w') as f:
+        f.write(copyright_in)
 
 if __name__ == '__main__':
     from optparse import OptionParser
