ab393a42331c48127232b783b8daf62f079b1fd3
Jan Wagner <waja@cyconet.org>
2018-11-07 15:59:13 +0100

    dsa/update-files.sh: Fix upstream URL

--- dsa/update-files.sh
+++ dsa/update-files.sh
@@ -3,7 +3,7 @@
 # update all files from the dsa nagios git
 find checks sbin share etc -type f | while read i; do
     tmp=`mktemp`
-    if wget -O "${tmp}" "http://anonscm.debian.org/gitweb/?p=mirror/dsa-nagios.git;a=blob_plain;f=dsa-nagios-checks/${i};hb=HEAD"; then
+    if wget -O "${tmp}" "https://salsa.debian.org/dsa-team/mirror/dsa-nagios/raw/master/dsa-nagios-checks/${i}"; then
         mv "${tmp}" "$i"
     else
         rm -f "${tmp}"
